bbeb8e860193d4edc37cf0772635e0dd57abbc83
stevembui52 <stevembui52@gmail.com>
2024-02-05 10:43:54 -0800

    try again

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 '''Contains a Flask web application API.
 '''
 import os
 from flask import Flask, jsonify
-=======
-"""
-Status of your API
-"""
-from api.v1.views import app_views
-from flask import Flask
->>>>>>> 916278987c8e95b783a5ac781038bea180e13b7a
 from flask_cors import CORS
 
 from models import storage
-<<<<<<< HEAD
 from api.v1.views import app_views
 
 
@@ -57,29 +48,3 @@ if __name__ == '__main__':
         port=app_port,
         threaded=True
     )
-=======
-import os
-
-app = Flask(__name__)
-
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """ json 404 page """
-    return {"error": "Not found"}, 404
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Close the database at the end of the request"""
-    storage.close()
-
-
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port, threaded=True)
->>>>>>> 916278987c8e95b783a5ac781038bea180e13b7a
